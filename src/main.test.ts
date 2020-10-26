9232566189a311d3a2bcb284905c255013b11258
Máron <maron.franca@gmail.com>
2020-10-26 00:44:18 -0300

    object test

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,86 @@
 import { isEmptyString, isNegativeNumber, isNegativeOrZeroNumber, isNil, isNotEmptyString, isNotNil } from "./validationCheck";
 
 
+interface ITestObj {
+    number?: {
+        optional?: number;
+        undefined: number | undefined;
+        null?: number | null;
+        nil?: number | null | undefined;
+        zero: number;
+        negative: number;
+        positive: number;
+        obj?: {};
+    } | null | undefined;
+    string?: {
+        optional?: string;
+        undefined: string | undefined;
+        null?: string | null;
+        nil?: string | null | undefined;
+        empty?: string | null | undefined;
+        notEmpty?: string | null | undefined;
+    } | null | undefined;
+    bool?: {
+        optional?: boolean;
+        undefined: boolean | undefined;
+        null?: boolean | null;
+        nil?: boolean | null | undefined;
+        true: boolean | null | undefined;
+        false: boolean | null | undefined;
+    } | null | undefined;
+}
+
 describe("Validation check", () => {
+    it("should validate object and mutate types correctly", () => {
+        const obj: ITestObj = {
+            number: {
+                zero: 0,
+                negative: -1,
+                undefined: undefined,
+                null: null,
+                positive: 1,
+                obj: {}
+            },
+            string: {
+                undefined: undefined,
+                null: null,
+                empty: "",
+                notEmpty: "Not empty"
+            },
+            bool: {
+                undefined: undefined,
+                null: null,
+                true: true,
+                false: false
+            }
+        };
+
+        expect(isNil(obj.number)).toBe(false);
+
+        if (isNotNil(obj.string)) {
+            expect(isEmptyString(obj.string.null)).toBe(true);
+            expect(isEmptyString(obj.string.undefined)).toBe(true);
+            expect(isEmptyString(obj.string.empty)).toBe(true);
+            expect(isEmptyString(obj.string.notEmpty)).toBe(false);
+        }
+
+        if (isNotNil(obj.number)) {
+            expect(isNil(obj.number.null)).toBe(true);
+            expect(isNil(obj.number.undefined)).toBe(true);
+            expect(isNil(obj.number.obj)).toBe(false);
+
+            expect(isNegativeOrZeroNumber(obj.number.zero)).toBe(true);
+            expect(isNegativeOrZeroNumber(obj.number.negative)).toBe(true);
+            expect(isNegativeOrZeroNumber(obj.number.positive)).toBe(false);
+            expect(isNegativeOrZeroNumber(obj.number.undefined)).toBe(true);
+        };
+
+        if (isNotNil(obj.bool)) {
+            expect(isNil(obj.bool.false)).toBe(false);
+            expect(isNil(obj.bool.true)).toBe(false);
+        }
+    });
+
     it("isNil should validate any data type", () => {
         expect(isNil(null)).toBe(true);
         expect(isNil(undefined)).toBe(true);
